fee2f8c43d2e1829343a158a98ab38b63ed1d282
Nithin A S <nithinsajeevan092@gmail.com>
2024-08-31 16:41:00 +0530

    Added login auth with firebase DB

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRyIbNU_1ifvlmypAzNWY_OiYf9rmf7UQ",
+    authDomain: "healwellweb.firebaseapp.com",
+    projectId: "healwellweb",
+    storageBucket: "healwellweb.appspot.com",
+    messagingSenderId: "944196723348",
+    appId: "1:944196723348:web:5fc37a5b9d187fa0290a81"
+  };
+export {firebaseConfig};
